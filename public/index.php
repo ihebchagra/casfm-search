d6f6604a92dcded1bd1b1e31a963c441b7c9bd11
Iheb Chagra <ihebchagra@gmail.com>
2025-01-05 13:33:54 +0100

    fix bugs

--- public/index.php
+++ public/index.php
@@ -132,7 +132,7 @@
                 <h1 class="text-4xl text-center font-serif text-indigo-900 mb-8">Recherche CA-SFM</h1>
                 <p class="text-gray-700 mb-8 font-serif">Cherchez simplement dans les recommandations <b>CA-SFM de l'EUCAST version juin 2024 v1.0</b> comme vous le feriez sur Google. Tapez un mot ou une phrase pour trouver rapidement les informations microbiologiques dont vous avez besoin.</p>
                 <div class="mb-6">
-                    <input type="text" x-model="searchTerm" @input="search" placeholder="Rechercher dans le document..." class="w-full px-4 py-2 rounded border border-indigo-200 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 font-serif">
+                    <input type="text" x-model="searchTerm" @input.debounce="search" placeholder="Rechercher dans le document..." class="w-full px-4 py-2 rounded border border-indigo-200 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 font-serif">
                 </div>
                 <div class="space-y-4">
                     <template x-if="results.length === 0">
